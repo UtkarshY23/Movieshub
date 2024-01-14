689678a4923ca4ff170e7997dbde5d523ecb8a37
UtkarshY23 <yadavutkarsh318@gmail.com>
2024-01-14 18:22:55 +0530

    disable starrating

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import './index.css';
-// import App from './App';
-import StarRating from "./StarRating";
+import './index.css';
+import App from './App';
+// import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
-      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating maxRating={10} size={24}  defaultRating={3}/>
+      // message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    /> */}
+    {/* <StarRating maxRating={10} size={24}  defaultRating={3}/> */}
     {/* <StarRating /> */}
   </React.StrictMode>
 );
