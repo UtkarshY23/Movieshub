64cd9c41041cc4902ce9b82f5aa0646c2915bbd9
UtkarshY23 <yadavutkarsh318@gmail.com>
2024-01-16 22:23:34 +0530

    commit 2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+
     {/* <StarRating
       maxRating={5}
       // message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
