b8fad02224fd195aad3877956e7a3e8ce5674b8a
UtkarshY23 <yadavutkarsh318@gmail.com>
2024-01-25 18:36:26 +0530

    remove

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
-// import App from './App';
+
 import Appcopy from './App copy.js'
-// import StarRating from "./StarRating";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Appcopy/>
-   
-    {/* <StarRating
-      maxRating={5}
-      // message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
-    /> */}
+ 
     
    
   </React.StrictMode>
